27e8058321457fed4d3d524d0fc7df087fe4ba3a
Hai Do <dotronghai91@gmail.com>
2022-08-07 16:18:35 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello Hai Do </h1>
- 
+      <h2>I am trying to push this project to git hub</h2>
+
     </div>
   );
 }

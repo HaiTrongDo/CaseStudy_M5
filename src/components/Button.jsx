405d5a63cb4af882b4ac63890d089bdc28ef17d8
HaiTrongDo <Dotronghai91@gmail.com>
2022-08-11 14:53:30 +0700

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,27 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import { useStateContext } from "../contexts/ContextProvider";
+import { useStateContext } from '../contexts/ContextProvider';
 
-const Button = ({
-  icon,
-  bgColor,
-  color,
-  bgHoverColor,
-  size,
-  text,
-  borderRadius,
-  width,
-}) => {
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
   const { setIsClicked, initialState } = useStateContext();
-  console.log(text);
+
   return (
     <button
       type="button"
       onClick={() => setIsClicked(initialState)}
       style={{ backgroundColor: bgColor, color, borderRadius }}
-      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor} left-400`}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
-      {text}
+      {icon} {text}
     </button>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './index.css';
 import App from './App';
 import {ContextProvider} from './contexts/ContextProvider';
 
-
 ReactDOM.render(
-  <ContextProvider>
-    <App/>    
-  </ContextProvider>
-
-
-, document.getElementById('root'))
+    <React.StrictMode>
+        <ContextProvider>
+            <App/>
+        </ContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
